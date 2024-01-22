dcde52067a49fb6e70636ac6706caf7c4e6f6eb7
Theigrams <sy1909148@buaa.edu.cn>
2024-01-22 23:10:05 +0800

    update Chapter05

--- Chapter05/02_frozenlake_q_iteration.py
+++ Chapter05/02_frozenlake_q_iteration.py
@@ -4,7 +4,7 @@ import collections
 import gymnasium as gym
 from torch.utils.tensorboard import SummaryWriter
 
-ENV_NAME = "FrozenLake-v0"
+ENV_NAME = "FrozenLake-v1"
 # ENV_NAME = "FrozenLake8x8-v0"      # uncomment for larger version
 GAMMA = 0.9
 TEST_EPISODES = 20
@@ -13,7 +13,7 @@ TEST_EPISODES = 20
 class Agent:
     def __init__(self):
         self.env = gym.make(ENV_NAME)
-        self.state = self.env.reset()
+        self.state = self.env.reset()[0]
         self.rewards = collections.defaultdict(float)
         self.transits = collections.defaultdict(collections.Counter)
         self.values = collections.defaultdict(float)
@@ -21,10 +21,10 @@ class Agent:
     def play_n_random_steps(self, count):
         for _ in range(count):
             action = self.env.action_space.sample()
-            new_state, reward, is_done, _ = self.env.step(action)
+            new_state, reward, terminated, truncated, _ = self.env.step(action)
             self.rewards[(self.state, action, new_state)] = reward
             self.transits[(self.state, action)][new_state] += 1
-            self.state = self.env.reset() if is_done else new_state
+            self.state = self.env.reset()[0] if terminated or truncated else new_state
 
     def select_action(self, state):
         best_action, best_value = None, None
@@ -37,14 +37,14 @@ class Agent:
 
     def play_episode(self, env):
         total_reward = 0.0
-        state = env.reset()
+        state, _ = env.reset()
         while True:
             action = self.select_action(state)
-            new_state, reward, is_done, _ = env.step(action)
+            new_state, reward, terminated, truncated, _ = env.step(action)
             self.rewards[(state, action, new_state)] = reward
             self.transits[(state, action)][new_state] += 1
             total_reward += reward
-            if is_done:
+            if terminated or truncated:
                 break
             state = new_state
         return total_reward
